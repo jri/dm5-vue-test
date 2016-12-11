0a7bef03e107e1f7ad9c9c7e9e94edcfee573e82
Jörg Richter <jri@deepamehta.de>
2016-12-11 18:01:36 +0100

    Initial commit

---
+++ src/Inbox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="inbox">
+    <ul>
+      <li v-for="infobit in infobits">{{infobit.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      infobits: [
+        {
+          title: "TTIP"
+        },
+        {
+          title: "Demokratie"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Inbox from './Inbox.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    inbox: Inbox
+  }
+})
