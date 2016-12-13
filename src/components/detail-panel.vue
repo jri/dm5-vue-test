4cde86918e5098433fe37adfca67c26b48abe0ec
Jörg Richter <jri@deepamehta.de>
2016-12-13 15:47:17 +0100

    Infobit type definitions

--- src/components/detail-panel.vue
+++ src/components/detail-panel.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="detail-panel" v-if="infobit">
     <h3>{{infobit.title}}</h3>
-    <div v-html="infobit.details.text"></div>
+    <div class="field" v-for="detail in types[infobit.type].details">
+      <div class="label">{{detail.label}}</div>
+      <div v-if="detail.dataType == 'Text' ||
+                 detail.dataType == 'Number'">
+        {{infobit.details[detail.prop]}}
+      </div>
+      <div v-else-if="detail.dataType == 'HTML'" v-html="infobit.details[detail.prop]">
+      </div>
+      <div v-else class="error">
+        "{{detail.dataType}}" is an unsupported data type
+      </div>
   </div>
 </template>
 
@@ -11,7 +21,59 @@ import http from "axios"
 export default {
   data() {
     return {
-      infobit: undefined
+      infobit: undefined,
+      types: {
+        "infobits.type.infobit": {
+          titleLabel: "Infobit Title",
+          details: [
+            {
+              prop: "text",
+              label: "Notes",
+              dataType: "HTML"
+            }
+          ]
+        },
+        "infobits.type.person": {
+          titleLabel: "Person Name",
+          details: [
+            {
+              prop: "birthDate",
+              label: "Year of birth",
+              dataType: "Number"
+            },
+            {
+              prop: "birthPlace",
+              label: "Place of birth",
+              dataType: "Text"
+            },
+            {
+              prop: "text",
+              label: "Notes",
+              dataType: "HTML"
+            }
+          ]
+        },
+        "infobits.type.question": {
+          titleLabel: "Question",
+          details: [
+            {
+              prop: "text",
+              label: "Notes",
+              dataType: "HTML"
+            }
+          ]
+        },
+        "infobits.type.structural_element": {
+          titleLabel: "Structural Element",
+          details: [
+            {
+              prop: "text",
+              label: "Notes",
+              dataType: "HTML"
+            }
+          ]
+        }
+      }
     }
   },
   watch: {
@@ -28,5 +90,15 @@ export default {
   flex-basis: 30%;
   overflow: auto;
   background-color: white;
+}
+
+#detail-panel .field > .label {
+  font-size: 75%;
+  color: grey;
+}
+
+#detail-panel .error {
+  font-size: 75%;
+  color: red;
 }
 </style>
